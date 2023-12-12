3706e193f0340c9ae095841cb4b7133d4b445f1d
ahmed al-jwafi <ah12ed34@gmail.com>
2023-12-13 02:52:46 +0300

    send email test 1

---
+++ resources/views/TempletMail/test.blade.php
@@ -0,0 +1,3 @@
+Welcome {{$name}} 
+<br>
+tow line
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\MyEmail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(
+    '/testEmail',
+    function () {
+        $name = "ahmed";
+       Mail::to('ah12ed34@gmail.com')->send(new MyEmail($name));
+    });
