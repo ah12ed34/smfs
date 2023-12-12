8118b30b3a6212ab9c9acf334f2d903bda653b25
kamel <kamlalfwdy@gmail.com>
2023-12-13 00:21:26 +0300

    update welcome file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>StudentClub</title>
+        <title>SC</title>
         
     </head>
     <body >
-        <h1> كامل الفودعي </h1>
+        <h1 style="color: cyan"> Welcome back from laravel</h1>
     </body>
 </html>
