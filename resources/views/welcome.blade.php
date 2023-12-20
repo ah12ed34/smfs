2436ff8e926a4dfafc33b5c6198d5af79d74626e
ahmed al-jwafi <ah12ed34@gmail.com>
2023-12-21 01:11:19 +0300

    add sass and js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>SC</title>
+        @vite(['resources/js/app.js','resources/sass/app.scss'])
     </head>
     <body >
         <h1 class="wl"> Welcome back from laravel</h1>
