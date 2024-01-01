a1c32f3c0c71270098b42fdc2195f964b6850f36
ahmed al-jwafi <ah12ed34@gmail.com>
2024-01-02 02:18:36 +0300

    add icone bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,18 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Include Bootstrap CSS -->
+        <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+        <!-- Include Bootstrap Icons CSS -->
+        <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
 
         <title>SC</title>
         @vite(['resources/js/app.js','resources/sass/app.scss'])
     </head>
-    <body >
-        <div>
-            <center>
-                <span class=" border-bottom rounded-top border-primary btn"><---></span>
-            </center>
-            <form action="" method="POST" >
-                @csrf
-            </form>
+    <body>
+        <div class="sidebar">
+            <i class="bi bi-list"></i> <!-- Sidebar icon -->
         </div>
+    
+
     </body>
 </html>
