127fb7406d15cb05f26c1a55b23d5a11e3147dce
ahmed AL-Wajeeh <21164093@su.edu.ye>
2024-01-03 03:13:55 +0300

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Home|SFMS')
 @section('content')
 <div class="responsive"></div>
-<div class="card"></div>
-<div class="card" style="margin-left: 22px;"></div>    
+<div class="card"> <img src="{{ Vite::image('allocation (1).png') }}"  width="150px">
+    <div class="card-child-1"> Distributed System نظم تشغيل <br> تقنية معلومات - مستوى رابع<br>أ.منال العريقي
+    </div></div>
+<div class="card" style="margin-left: 22px;"> <img src="{{ Vite::image('allocation (1).png') }}"  width="150px">
+    <div class="card-child-1"> Networks Management إدارة شبكات <br> تقنية معلومات - مستوى رابع<br>أ.منال العريقي
+    </div></div>    
 @endsection
