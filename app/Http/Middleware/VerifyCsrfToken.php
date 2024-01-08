35536f86aa945064e98448a80ba5e35af75c31b5
ahmed al-jwafi <ah12ed34@gmail.com>
2024-01-08 17:38:07 +0300

    add The URIs that should be excluded from CSRF verification.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Routing\Route;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -13,5 +14,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+       '/logout',
     ];
 }
