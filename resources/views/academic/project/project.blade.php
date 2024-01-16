024ce380f3b021b5c3228d7c7548406c981f47aa
ahmed AL-Wajeeh <21164093@su.edu.ye>
2024-01-17 01:10:45 +0300

    update projects

--- resources/views/academic/project/project.blade.php
+++ resources/views/academic/project/project.blade.php
@@ -122,7 +122,7 @@
                 <!-- Modal Header -->
                 <div class="modal-header" id="modheader">
                     انشاء مشروع جديد
-                    <button type="button"  data-dismiss="modal"><img src="{{Vite::image("cancelbtn.png")}}"   width="15px" ></button>
+                    <button type="button"  data-dismiss="modal"><img src="{{Vite::image("cancelbtn.png")}}"   width="20px" ></button>
 
                 </div>
 
