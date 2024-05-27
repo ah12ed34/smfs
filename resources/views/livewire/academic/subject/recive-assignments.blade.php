3fb03babe5e130b585de36b0a7760773ef51e977
ahmed AL-Wajeeh <21164093@su.edu.ye>
2024-05-28 02:12:21 +0300

    update academic recieve assignents students page

--- resources/views/livewire/academic/subject/recive-assignments.blade.php
+++ resources/views/livewire/academic/subject/recive-assignments.blade.php
@@ -1,180 +1,146 @@
-    @section('nav')
-        @livewire('components.nav.academic.subject.recive-assignments'
-        ,['group_subject'=>$group_subject,'tabActive'=>$tabActive])
-    @endSection
+@section('nav')
+@livewire('components.nav.academic.subject.recive-assignments'
+,['group_subject'=>$group_subject])
+@endSection
 <div>
 
-    <div class="responsive"></div>
-    <div class="container" id="container-project" style="  padding-top: 30px;" >
+<div class="responsive"></div>
+<div class="container" id="container-project" style="  padding-top: 30px;" >
 
 
-        <div class="table-responsive-xl">
-            <table class="table" id="table" style=" margin-right: -30px; " >
-                <thead class="table-header" style="font-size: 12px;">
-                    <tr class="table-light" id="modldetials">
-                        <th>تصحيح التكليف </th>
-                        <th>ملاحظة المدرس</th>
-                        <th>الدرجة</th>
-                        <th>الملف</th>
-                        <th> حالة التسليم</th>
-                        <th>تاريخ التسليم</th>
-                        <th> اسم الطالب</th>
-                    </tr>
-                </thead>
-                <tbody>
-<<<<<<< HEAD
-                    <tr class="table-light" id="modldetials" style="margin-top:7px;">
-                        <td><button type="submit" class="btn btn-primary btn-sm" id="btn-detials" data-toggle="modal" data-target="#ModaldCheckAssignmentsStudents">تصحيح التكليف</button> </td>
-=======
-                    @forelse ($deliverys as $delivery)
-                    {{-- @dd($delivery) --}}
-                        <tr class="table-light" id="modldetials">
-                            <td>{{ $delivery->comment }}</td>
-                            <td>{{ $delivery->grade }}</td>
-                            <td>
-                                @if($delivery->file)
-                                <a wire:click='download({{ $delivery->id }})' style="
-                                    color: #007bff;
-                                    text-decoration: none;
-                                    cursor: pointer;
-                                ">
-                                    <i class="bi bi-download"></i>
-                                </a>
-                                @else
-                                    <i class="bi bi-file-x"></i>
-                                @endif
-                            </td>
-                            <td>{{ $delivery->status }}</td>
-                            <td>{{ $delivery->delivery_date }}</td>
-                            <td>{{ $delivery->student
-                             }}</td>
-                        </tr>
-                    @empty
+<div class="table-responsive-xl">
+    <table class="table" id="table" style=" margin-right: -30px; " >
+        <thead class="table-header" style="font-size: 12px;">
+            <tr class="table-light" id="modldetials">
+                <th>تصحيح التكليف </th>
+                <th>ملاحظة المدرس</th>
+                <th>الدرجة</th>
+                <th>الملف</th>
+                <th> حالة التسليم</th>
+                <th>تاريخ التسليم</th>
+                <th> اسم الطالب</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table-light" id="modldetials" style="margin-top:7px;">
+                <td><button type="submit" class="btn btn-primary btn-sm" id="btn-detials" data-toggle="modal" data-target="#ModaldCheckAssignmentsStudents">تصحيح التكليف</button> </td>
+                <td>*******</td>
+                <td style="width: 15%;"><input type="text" class="form-control input_gradeAssingnments" name="gradeAssingnments" ></td>
+                <td> ***</td>
+                <td>*******</td>
+                <td>***** </td>
+                <td>SFMS</td>
+            </tr>
+            <tr class="table-light">
+                <td><button type="submit" class="btn btn-primary btn-sm" id="btn-detials" data-toggle="modal" data-target="#ModaldCheckAssignmentsStudents">تصحيح التكليف</button> </td>
+                <td>*******</td>
+                <td><input type="text" class="form-control input_gradeAssingnments" name="gradeAssingnments"></td>
+                <td> ***</td>
+                <td>*******</td>
+                <td>***** </td>
+                <td>SFMS</td>
+            </tr>
+            <tr class="table-light">
+                <td><button type="submit" class="btn btn-primary btn-sm" id="btn-detials" data-toggle="modal" data-target="#ModaldCheckAssignmentsStudents">تصحيح التكليف</button> </td>
+                <td>*******</td>
+                <td><input type="text" class="form-control input_gradeAssingnments" name="gradeAssingnments"></td>
+                <td> ***</td>
+                <td>*******</td>
+                <td>***** </td>
+                <td>SFMS</td>
+            </tr>
+            <tr class="table-light">
+                <td><button type="submit" class="btn btn-primary btn-sm" id="btn-detials" data-toggle="modal" data-target="#ModaldCheckAssignmentsStudents">تصحيح التكليف</button> </td>
+                <td>*******</td>
+                <td><input type="text" class="form-control input_gradeAssingnments" name="gradeAssingnments"></td>
+                <td> ***</td>
+                <td>*******</td>
+                <td>***** </td>
+                <td>SFMS</td>
+            </tr>
 
-                    @endforelse
-                    {{-- <tr class="table-light" id="modldetials" style="margin-top:7px;">
+        </tbody>
+    </table>
+</div>
+</div>
 
->>>>>>> a7e7d87edc6937d51701314dbc8de3116faa884f
-                        <td>*******</td>
-                        <td style="width: 15%;"><input type="text" class="form-control input_gradeAssingnments" name="gradeAssingnments" ></td>
-                        <td> ***</td>
-                        <td>*******</td>
-                        <td>***** </td>
-                        <td>SFMS</td>
-                    </tr>
-                    <tr class="table-light">
-                        <td><button type="submit" class="btn btn-primary btn-sm" id="btn-detials" data-toggle="modal" data-target="#ModaldCheckAssignmentsStudents">تصحيح التكليف</button> </td>
-                        <td>*******</td>
-                        <td><input type="text" class="form-control input_gradeAssingnments" name="gradeAssingnments"></td>
-                        <td> ***</td>
-                        <td>*******</td>
-                        <td>***** </td>
-                        <td>SFMS</td>
-                    </tr>
-                    <tr class="table-light">
-                        <td><button type="submit" class="btn btn-primary btn-sm" id="btn-detials" data-toggle="modal" data-target="#ModaldCheckAssignmentsStudents">تصحيح التكليف</button> </td>
-                        <td>*******</td>
-                        <td><input type="text" class="form-control input_gradeAssingnments" name="gradeAssingnments"></td>
-                        <td> ***</td>
-                        <td>*******</td>
-                        <td>***** </td>
-                        <td>SFMS</td>
-                    </tr>
-                    <tr class="table-light">
-                        <td><button type="submit" class="btn btn-primary btn-sm" id="btn-detials" data-toggle="modal" data-target="#ModaldCheckAssignmentsStudents">تصحيح التكليف</button> </td>
-                        <td>*******</td>
-                        <td><input type="text" class="form-control input_gradeAssingnments" name="gradeAssingnments"></td>
-                        <td> ***</td>
-                        <td>*******</td>
-                        <td>***** </td>
-                        <td>SFMS</td>
-                    </tr> --}}
-
-                </tbody>
-            </table>
-        </div>
-        <nav>
-            {{ $deliverys->links(myapp::viewPagination) }}
-        </nav>
-    </div>
-
-    <!-- The ModalDetailsStudents -->
+<!-- The ModalDetailsStudents -->
 <div class="modal fade" id="ModaldCheckAssignmentsStudents">
-    <div class="modal-dialog ">
-        <div class="modal-content ModaldDetailsAcademic" id="modal-content" style="background-color: #F6F7FA; height:600px;">
+<div class="modal-dialog ">
+<div class="modal-content ModaldDetailsAcademic" id="modal-content" style="background-color: #F6F7FA; height:600px;">
 
-            <!-- Modal Header -->
-            <div class="modal-header ModaldDetailsAcademic" id="modheader">
-                 التفاصيل
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-
-            <!-- Modal body -->
-            <div class="modal-body ModaldDetailsAcademic">
-                <form action="/action_page.php" style="display: block;">
-                    <div class="form-group">
-                        <div class="table-responsive ">
-                            <table class="table details-academic " style="width:100%;;" dir="rtl">
-                                        <tr class="table-light" id="modldetials">
-                                            <th style=" width:25%; "> الرقم الأكاديمي</th>
-                                            <td>**********</td>
-                                        </tr>
-                                        <tr class="table-light " id="modldetials">
-                                            <th style=" width:25%; "> اسم الطالب</th>
-                                            <td>**********</td>
-                                        </tr>
-                                        <tr class="table-light" id="modldetials">
-                                            <th style="width: 25%;"> المجموعة</th>
-                                            <td>**********</td>
-                                        </tr>
-                                        <tr class="table-light" id="modldetials">
-                                            <th style="width: 25%;">  تاريخ التسليم </th>
-                                            <td>**********</td>
-                                        </tr>
-                                        <tr class="table-light" id="modldetials">
-                                            <th style="width: 25%;" >  حالة التسليم </th>
-                                            <td>**********</td>
-                                        </tr>
-                                        <tr class="table-light" id="modldetials">
-                                            <th style="width: 25%;">  الملف</th>
-                                            <td>**********</td>
-                                        </tr>
-                                        <tr class="table-light" id="modldetials">
-                                            <th style="width: 25%;"> ملاحظة الطالب</th>
-                                            <td>**********</td>
-                                        </tr>
-
-                            </table>
-                        </div>
-
-                        <div class="table-responsive ">
-                            <table class="table details-academic " style="width:100%; margin-top:10px;" dir="rtl">
-                                        {{-- <tr  id="modldetials">
-                                            <th colspan="2" > التصحيح </th>
-                                        </tr> --}}
-                                        <tr  id="modldetials">
-                                            <th style=" width:25%; "> الدرجة </th>
-                                            <td> <input class="form-control" type="data" ></td>
-                                        </tr>
-                                        <tr  id="modldetials">
-                                            <th style="width: 25%;">  ملاحظة المدرس</th>
-                                            <td><textarea class="form-control" name="note" id="" cols="20" rows="4" style="height: 100%"></textarea></td>
-                                        </tr>
-
-                            </table>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-            <!-- Modal footer -->
-
-            <div class="modal-footer" style="">
-               <button type="submit" class="btn btn-primary" id="btnsave">حفظ</button>
-                <button type="button" class="btn btn-danger" data-dismiss="modal" id="btncancel">إلغاء</button>
-            </div>
-        </div>
+    <!-- Modal Header -->
+    <div class="modal-header ModaldDetailsAcademic" id="modheader">
+         التفاصيل
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
     </div>
+
+    <!-- Modal body -->
+    <div class="modal-body ModaldDetailsAcademic">
+        <form action="/action_page.php" style="display: block;">
+            <div class="form-group">
+                <div class="table-responsive ">
+                    <table class="table details-academic " style="width:100%;;" dir="rtl">
+                                <tr class="table-light" id="modldetials">
+                                    <th style=" width:25%; "> الرقم الأكاديمي</th>
+                                    <td>**********</td>
+                                </tr>
+                                <tr class="table-light " id="modldetials">
+                                    <th style=" width:25%; "> اسم الطالب</th>
+                                    <td>**********</td>
+                                </tr>
+                                <tr class="table-light" id="modldetials">
+                                    <th style="width: 25%;"> المجموعة</th>
+                                    <td>**********</td>
+                                </tr>
+                                <tr class="table-light" id="modldetials">
+                                    <th style="width: 25%;">  تاريخ التسليم </th>
+                                    <td>**********</td>
+                                </tr>
+                                <tr class="table-light" id="modldetials">
+                                    <th style="width: 25%;" >  حالة التسليم </th>
+                                    <td>**********</td>
+                                </tr>
+                                <tr class="table-light" id="modldetials">
+                                    <th style="width: 25%;">  الملف</th>
+                                    <td>**********</td>
+                                </tr>
+                                <tr class="table-light" id="modldetials">
+                                    <th style="width: 25%;"> ملاحظة الطالب</th>
+                                    <td>**********</td>
+                                </tr>
+
+                    </table>
+                </div>
+
+                <div class="table-responsive ">
+                    <table class="table details-academic " style="width:100%; margin-top:10px;" dir="rtl">
+                                {{-- <tr  id="modldetials">
+                                    <th colspan="2" > التصحيح </th>
+                                </tr> --}}
+                                <tr  id="modldetials">
+                                    <th style=" width:25%; "> الدرجة </th>
+                                    <td> <input class="form-control" type="data" ></td>
+                                </tr>
+                                <tr  id="modldetials">
+                                    <th style="width: 25%;">  ملاحظة المدرس</th>
+                                    <td><textarea class="form-control" name="note" id="" cols="20" rows="4" style="height: 100%"></textarea></td>
+                                </tr>
+
+                    </table>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- Modal footer -->
+
+    <div class="modal-footer" style="">
+       <button type="submit" class="btn btn-primary" id="btnsave">حفظ</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal" id="btncancel">إلغاء</button>
+    </div>
+</div>
+</div>
 </div>
 </div>
 
